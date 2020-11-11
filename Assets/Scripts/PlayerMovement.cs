5d85902ed022e3044097e7c2b0d8dc8ffffa089e
Will East <we2g18@soton.ac.uk>
2020-11-11 03:09:10 +0000

    Initial Level Design
    - Primary scene set up
    - Player scripting updated from prototype
    - Basic tilemap created
    - Rough level design started
    - Spike functionality tested
    - Unlock functionality tested
    - Follow camera implemented
    - Canvas added

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,90 @@ using TMPro;
 
 public class PlayerMovement : MonoBehaviour
 {
+    
+    public float movementSpeed;
+    public Rigidbody2D rb;
+    public SpriteRenderer spriteRenderer;
+    public Sprite[] spriteArray;
+    public float jumpForce = 10f;
+    public TextMeshProUGUI deathText;
+
+    private float mx;
+    private BoxCollider2D boxCollider;
+
+
+    private bool isGrounded = false;
+    private bool canMove = true;
+
+    private void Start()
+    {
+        spriteRenderer.sprite = spriteArray[0];
+        boxCollider = gameObject.AddComponent<BoxCollider2D>();
+        deathText.gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (canMove)
+        {
+            mx = Input.GetAxis("Horizontal");
+
+            if (Input.GetButtonDown("Jump") && isGrounded)
+            {
+                Jump();
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (canMove)
+        {
+            Vector2 movement = new Vector2(mx * movementSpeed, rb.velocity.y);
+            rb.velocity = movement;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        } else if (collision.gameObject.CompareTag("Spike"))
+        {
+            canMove = false;
+            deathText.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Stair Trigger")){
+            StartCoroutine(UnlockStairs());
+        }
+    }
+
+    void Jump()
+    {
+        Vector2 movement = new Vector2(rb.velocity.x, jumpForce);
+
+        rb.velocity = movement;
+    }
+
+    IEnumerator UnlockStairs()
+    {
+        yield return new WaitForSeconds(5);
+        GameObject.FindGameObjectWithTag("Stair Block").SetActive(false);
+    }
+    /*LEGACY CODE FROM PROOF OF CONCEPT PROTOTYPE. DELETE WHEN READY.
     public float movementSpeed;
     public Rigidbody2D rb;
 
@@ -115,10 +199,10 @@ public class PlayerMovement : MonoBehaviour
         yield return new WaitForSeconds(delay);
         foundText.gameObject.SetActive(false);
     }
-
+    
     void ResetCollider()
     {
         boxCollider = null;
         boxCollider = gameObject.AddComponent<BoxCollider2D>();
-    }
+    }*/
 }
