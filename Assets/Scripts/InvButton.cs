2f1cb5ff8730cc0a359baaf4318836680fc0b311
Robert106 <bobs.hall@hotmail.com>
2020-11-13 17:21:40 +0000

    Added button to open/close the inventory

---
+++ Assets/Scripts/InvButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InvButton : MonoBehaviour
+{
+    public UIInventory inventory;
+    public void OnPointerClick(PointerEventData eventData) {
+        Debug.Log("Clicked");
+        inventory.gameObject.SetActive(true);
+    }
+}
